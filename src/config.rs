ac8b99e9cbcaf60e79149171cf627b88206d00d5
Ilshat Sultanov <ilshat@sultanov.team>
2024-03-07 01:15:32 +0300

    Extract constants and update docstrings

--- src/config.rs
+++ src/config.rs
@@ -1,5 +1,10 @@
-use dirs;
 use std::{io, path};
+
+use dirs;
+
+const DOTTY_ROOT_PATH: &str = ".dotty";
+const DOTTY_CONFIG_PATH: &str = ".config/dotty";
+const DOTTY_CONFIG_FILENAME: &str = "config.toml";
 
 /// Returns the path to the `$HOME/.dotty` directory.
 pub fn root_path() -> io::Result<path::PathBuf> {
@@ -10,14 +15,21 @@ pub fn root_path() -> io::Result<path::PathBuf> {
         )
     })?;
 
-    let root = home.join(".dotty");
+    let root = home.join(DOTTY_ROOT_PATH);
 
     Ok(root)
 }
 
-/// Returns the path to the `$HOME/.dotty/.config/dotty/config.toml` file
+/// Returns the path to the `$HOME/.dotty/.config/dotty` directory.
 pub fn config_path() -> io::Result<path::PathBuf> {
     let root = root_path()?;
-    let config = root.join(".config/dotty/config.toml");
+    let config = root.join(DOTTY_CONFIG_PATH);
+    Ok(config)
+}
+
+/// Returns the path to the `$HOME/.dotty/.config/dotty/config.toml` file.
+pub fn config_file_path() -> io::Result<path::PathBuf> {
+    let root = config_path()?;
+    let config = root.join(DOTTY_CONFIG_FILENAME);
     Ok(config)
 }
