c40e40ccf862974a3303e9f7feaeb53fc1ea52e6
Ilshat Sultanov <ilshat@sultanov.team>
2024-03-07 01:23:00 +0300

    Simplify helpers

--- src/config.rs
+++ src/config.rs
@@ -8,28 +8,26 @@ const DOTTY_CONFIG_FILENAME: &str = "config.toml";
 
 /// Returns the path to the `$HOME/.dotty` directory.
 pub fn root_path() -> io::Result<path::PathBuf> {
-    let home = dirs::home_dir().ok_or_else(|| {
-        io::Error::new(
-            io::ErrorKind::NotFound,
-            "The `home` directory was not found. Please set `$HOME`.",
-        )
-    })?;
+    let path = dirs::home_dir()
+        .ok_or_else(|| {
+            io::Error::new(
+                io::ErrorKind::NotFound,
+                "The `home` directory was not found. Please set `$HOME`.",
+            )
+        })?
+        .join(DOTTY_ROOT_PATH);
 
-    let root = home.join(DOTTY_ROOT_PATH);
-
-    Ok(root)
+    Ok(path)
 }
 
 /// Returns the path to the `$HOME/.dotty/.config/dotty` directory.
 pub fn config_path() -> io::Result<path::PathBuf> {
-    let root = root_path()?;
-    let config = root.join(DOTTY_CONFIG_PATH);
-    Ok(config)
+    let path = root_path()?.join(DOTTY_CONFIG_PATH);
+    Ok(path)
 }
 
 /// Returns the path to the `$HOME/.dotty/.config/dotty/config.toml` file.
 pub fn config_file_path() -> io::Result<path::PathBuf> {
-    let root = config_path()?;
-    let config = root.join(DOTTY_CONFIG_FILENAME);
-    Ok(config)
+    let path = config_path()?.join(DOTTY_CONFIG_FILENAME);
+    Ok(path)
 }
